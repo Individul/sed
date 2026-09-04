e9eeaeabf4d5d8cae108ad3a375e348d8cdc5f30
Individul <prisacarudumitru@gmail.com>
2026-09-04 20:48:02 +0300

    Refactor open-next.config.ts to remove Cloudflare config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,4 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
+// @ts-nocheck
+export default {
   dangerouslyUseUnsupportedNextVersion: true,
-});
+};
